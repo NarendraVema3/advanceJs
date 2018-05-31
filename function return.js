b741968b446a87cdcb0c83bd9fce12734f3b1515
NarendraVema3 <36954999+NarendraVema3@users.noreply.github.com>
2018-05-31 16:51:43 +0530

    Create function return.js

---
+++ function return.js
@@ -0,0 +1,29 @@
+
+
+function interviewQuestions()
+{
+   if(job == teacher)
+   {
+      retrun funtion(name)
+      {
+         consoel.log('you are doing teacher job ',name);
+      }
+   }
+   elseif( job == software)
+   {
+       return function(name)
+       {
+         console.log('you are doing software jog',name)
+       }
+   }
+ 
+}
+
+
+var teacher = interviewQuestions(teacher);
+var software = interviewQuestions(software);
+
+
+teacher('narendra');
+
+software('nari');
