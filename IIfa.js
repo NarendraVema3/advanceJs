01a5267972b5f0cf83967affbd44a077c0c2af85
NarendraVema3 <36954999+NarendraVema3@users.noreply.github.com>
2018-05-31 16:55:01 +0530

    Create IIfa.js

---
+++ IIfa.js
@@ -0,0 +1,19 @@
+
+(function()
+{
+    var score = math.random()*10;
+    console.log(score > 10);
+})
+
+(); //function calling
+
+
+
+
+(function (scr)
+{
+  var score = scr;
+    console.log(scr);
+})
+
+(scr);
